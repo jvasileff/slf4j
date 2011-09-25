5c741a97e19a909e3a664d01af866632f96352be
John Vasileff <git@netcc.us>
2011-09-25 11:33:09 -0400

    optimize EventMessage

--- slf4j-ext/src/main/java/org/slf4j/ext/EventMessage.java
+++ slf4j-ext/src/main/java/org/slf4j/ext/EventMessage.java
@@ -4,14 +4,20 @@ import org.slf4j.messages.ParameterizedMessage;
 
 public class EventMessage implements ParameterizedMessage {
 
+  private static final long serialVersionUID = -6745923052345545500L;
+
   private final EventData eventData;
-  
+  private transient String formattedMessage;
+
   public EventMessage(EventData eventData) {
     this.eventData = eventData;
   }
 
   public String getFormattedMessage() {
-    return eventData.toXML();
+    if (formattedMessage == null) {
+        formattedMessage = eventData.toXML();
+    }
+    return formattedMessage;
   }
 
   public Object[] getParameters() {
